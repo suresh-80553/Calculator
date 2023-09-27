f66c5f1219ab133033fcef59cb989ffb3ca18522
suresh-80553 <128765977+suresh-80553@users.noreply.github.com>
2023-09-27 14:50:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("Display").value="";
+function insertElem(num){
+    return document.getElementById("Display").value+=num;
+}
+
+
+function Equal(){
+    return document.getElementById("Display").value=eval(document.getElementById("Display").value);
+}
+
+function Clear(){
+    return document.getElementById("Display").value="";
+}
